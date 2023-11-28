d72ce52656d74f0a95b77b1cb1a0fd392d43de0d
jacopograndi <jacopo.grandi@outlook.it>
2023-11-28 21:30:30 +0100

    less mess

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,21 @@ use bevy::{
     asset::LoadState,
     core_pipeline::fxaa::Fxaa,
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
-    pbr::wireframe::{Wireframe, WireframePlugin},
     prelude::*,
-    render::{
-        mesh::{Indices, VertexAttributeValues},
-        render_resource::PrimitiveTopology,
-    },
     window::{PresentMode, WindowPlugin},
 };
 
-use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use bevy_flycam::prelude::*;
 
-use block_mesh::{
-    greedy_quads,
-    ndshape::{ConstShape, ConstShape3u32},
-    visible_block_faces,
+mod voxels;
+use voxels::{
+    voxel_world::ArcGridHierarchy, BevyVoxelEnginePlugin, LoadVoxelWorld, VoxelCameraBundle,
 };
 
-mod instanced_material;
-use instanced_material::*;
-
-mod raycast;
-
 mod voxel_shapes;
-use voxel_engine::{BevyVoxelEnginePlugin, LoadVoxelWorld, VoxelCameraBundle};
-use voxel_pipeline::voxel_world::NewGH;
 use voxel_shapes::*;
 
-mod voxel_engine;
-mod voxel_pipeline;
+use crate::voxels::raycast;
 
 fn main() {
     let mut app = App::new();
@@ -48,40 +33,32 @@ fn main() {
         FrameTimeDiagnosticsPlugin,
         LogDiagnosticsPlugin::default(),
         NoCameraPlayerPlugin,
-        InstancedMaterialPlugin,
-        EguiPlugin,
-        WireframePlugin,
         BevyVoxelEnginePlugin,
     ))
     .insert_resource(ClearColor(Color::MIDNIGHT_BLUE))
     .insert_resource(Handles::default())
     .add_state::<FlowState>()
     .add_systems(Startup, load)
+    .add_systems(OnEnter(FlowState::Base), setup)
     .add_systems(Update, check_loading.run_if(in_state(FlowState::Loading)))
-    .add_systems(OnEnter(FlowState::Base), (setup, start_benchmark))
-    .add_systems(OnEnter(FlowState::Benchmark), setup_bench)
-    .add_systems(OnExit(FlowState::Benchmark), teardown_bench)
     .add_systems(Update, print_mesh_count)
-    .add_systems(Update, wireframe)
-    .add_systems(Update, voxel_break)
-    .add_systems(OnEnter(FlowState::Transition), start_benchmark)
-    .add_event::<ToggleWireframeEvent>();
+    .add_systems(Update, voxel_break);
 
     bevy_mod_debugdump::print_render_graph(&mut app);
 
     app.run();
 }
 
+// just for prototype
 fn voxel_break(
     camera_query: Query<(&Camera, &Transform)>,
-    mut new_gh: ResMut<NewGH>,
-    keys: Res<Input<KeyCode>>,
+    mut new_gh: ResMut<ArcGridHierarchy>,
+    mouse: Res<Input<MouseButton>>,
 ) {
     if let Ok((_cam, tr)) = camera_query.get_single() {
-        if let NewGH::Some(newgh) = new_gh.as_mut().clone() {
+        if let ArcGridHierarchy::Some(newgh) = new_gh.as_mut().clone() {
             let mut gh = newgh.lock().unwrap();
-            //j todo bug 
-            println!("{:?}", tr.forward());
+            //j todo bug
             let s = gh.texture_size as i32;
             let grid = Grid::from_vec(
                 gh.raw
@@ -89,12 +66,40 @@ fn voxel_break(
                     .map(|pos| *pos - IVec3::splat(s / 2))
                     .collect(),
             );
-            let (pos, dist) = raycast::raycast(tr.translation * 4., tr.forward(), &grid);
-            let pos = pos + IVec3::splat(s / 2);
-            if dist.is_finite() && gh.raw.contains(&pos) {
-                let i = (pos.z + pos.y * s + pos.x * s * s) * 2;
-                gh.texture_data[i as usize] = 0;
-                gh.raw.retain(|p| p != &pos);
+            #[derive(PartialEq)]
+            enum Act {
+                PlaceBlock,
+                RemoveBlock,
+            }
+            let act = match (
+                mouse.pressed(MouseButton::Left),
+                mouse.pressed(MouseButton::Right),
+            ) {
+                (true, false) => Some(Act::PlaceBlock),
+                (false, true) => Some(Act::RemoveBlock),
+                _ => None,
+            };
+            if let Some(act) = act {
+                if let Some((pos, norm, dist)) =
+                    raycast::raycast(tr.translation * 4., tr.forward(), &grid)
+                {
+                    let pos = pos + IVec3::splat(s / 2);
+                    if dist.is_finite() && gh.raw.contains(&pos) {
+                        match act {
+                            Act::RemoveBlock => {
+                                let i = (pos.z + pos.y * s + pos.x * s * s) * 2;
+                                gh.texture_data[i as usize] = 0;
+                                gh.raw.retain(|p| p != &pos);
+                            }
+                            Act::PlaceBlock => {
+                                let pos = pos - norm;
+                                let i = (pos.z + pos.y * s + pos.x * s * s) * 2;
+                                gh.texture_data[i as usize] = 1;
+                                gh.raw.push(pos);
+                            }
+                        };
+                    }
+                }
             }
         }
     }
@@ -105,92 +110,10 @@ enum FlowState {
     #[default]
     Loading,
     Base,
-    Benchmark,
-    Transition,
-}
-
-fn ui(
-    mut contexts: EguiContexts,
-    mut method: ResMut<RenderMethod>,
-    mut shape: ResMut<VoxelShape>,
-    mut next_state: ResMut<NextState<FlowState>>,
-    mut settings: ResMut<Settings>,
-    mut toggle_wireframe: EventWriter<ToggleWireframeEvent>,
-) {
-    egui::SidePanel::new(egui::panel::Side::Right, "Benchmark").show(contexts.ctx_mut(), |ui| {
-        ui.separator();
-        ui.label("CHOOSE RENDER METHOD");
-        for m in RenderMethod::opts() {
-            let mut sel = m == *method;
-            if ui.toggle_value(&mut sel, format!("{:?}", m)).clicked() {
-                *method = m.clone();
-                next_state.set(FlowState::Transition);
-            }
-        }
-        ui.separator();
-        ui.label("CHOOSE VOXEL SHAPE");
-        for s in VoxelShape::opts() {
-            let mut sel = s == *shape;
-            if ui.toggle_value(&mut sel, format!("{:?}", s)).clicked() {
-                *shape = s.clone();
-                next_state.set(FlowState::Transition);
-            }
-        }
-        ui.separator();
-        ui.label("RENDER SETTINGS");
-        if ui
-            .toggle_value(&mut settings.wireframe, "wireframe")
-            .changed()
-        {
-            toggle_wireframe.send(ToggleWireframeEvent {
-                active: settings.wireframe,
-            })
-        }
-        ui.separator();
-        ui.label("COMMANDS");
-        ui.label("Press esc to use the mouse");
-        ui.label("WASD to move in xz plane");
-        ui.label("EQ to move along y axis");
-    });
-}
-
-#[derive(Resource)]
-struct Settings {
-    wireframe: bool,
-}
-impl Default for Settings {
-    fn default() -> Self {
-        Self { wireframe: false }
-    }
-}
-
-#[derive(Event)]
-struct ToggleWireframeEvent {
-    active: bool,
-}
-
-fn wireframe(
-    mut commands: Commands,
-    query: Query<(Entity, &BenchedMesh)>,
-    mut event: EventReader<ToggleWireframeEvent>,
-) {
-    for event in event.read() {
-        if event.active {
-            for (ent, _) in query.iter() {
-                commands.entity(ent).insert(Wireframe);
-            }
-        } else {
-            for (ent, _) in query.iter() {
-                commands.entity(ent).remove::<Wireframe>();
-            }
-        }
-    }
 }
 
 #[derive(Resource, Default)]
 struct Handles {
-    material: Handle<StandardMaterial>,
-    cube: Handle<Mesh>,
     texture_blocks: Handle<Image>,
 }
 
@@ -209,29 +132,11 @@ fn check_loading(
     }
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut material_assets: ResMut<Assets<StandardMaterial>>,
-    mut handles: ResMut<Handles>,
-    mut load_voxel_world: ResMut<LoadVoxelWorld>,
-) {
-    commands.insert_resource(Settings::default());
+fn setup(mut commands: Commands, mut load_voxel_world: ResMut<LoadVoxelWorld>) {
     commands.insert_resource(VoxelShape::default());
-    commands.insert_resource(RenderMethod::default());
-    commands.spawn(DirectionalLightBundle { ..default() });
-    /*
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform {
-                translation: Vec3::new(60.0, 16.0, 312.0),
-                ..Default::default()
-            },
-            ..default()
-        },
-        FlyCam,
-    ));
-    */
+
+    // bevy-fly-cam camera settings
+    // bevy-fly-cam is prototype only
     commands.insert_resource(MovementSettings {
         sensitivity: 0.00015,
         speed: 30.0,
@@ -241,75 +146,11 @@ fn setup(
         move_descend: KeyCode::Q,
         ..Default::default()
     });
-    handles.material = material_assets.add(StandardMaterial {
-        unlit: true,
-        //base_color: Color::rgba(0.5, 0.2, 0.0, 0.5),
-        //alpha_mode: AlphaMode::Add,
-        base_color_texture: Some(handles.texture_blocks.clone()),
-        ..default()
-    });
-    let sp = shape::Box::new(1., 1., 1.);
-    // suppose Y-up right hand, and camera look from +z to -z
-    let vertices = &[
-        // Front
-        ([sp.min_x, sp.min_y, sp.max_z], [0., 0., 1.0], [0., 0.]),
-        ([sp.max_x, sp.min_y, sp.max_z], [0., 0., 1.0], [1.0, 0.]),
-        ([sp.max_x, sp.max_y, sp.max_z], [0., 0., 1.0], [1.0, 1.0]),
-        ([sp.min_x, sp.max_y, sp.max_z], [0., 0., 1.0], [0., 1.0]),
-        // Back
-        ([sp.min_x, sp.max_y, sp.min_z], [0., 0., -1.0], [1.0, 0.]),
-        ([sp.max_x, sp.max_y, sp.min_z], [0., 0., -1.0], [0., 0.]),
-        ([sp.max_x, sp.min_y, sp.min_z], [0., 0., -1.0], [0., 1.0]),
-        ([sp.min_x, sp.min_y, sp.min_z], [0., 0., -1.0], [1.0, 1.0]),
-        // Right
-        ([sp.max_x, sp.min_y, sp.min_z], [1.0, 0., 0.], [0., 0.]),
-        ([sp.max_x, sp.max_y, sp.min_z], [1.0, 0., 0.], [1.0, 0.]),
-        ([sp.max_x, sp.max_y, sp.max_z], [1.0, 0., 0.], [1.0, 1.0]),
-        ([sp.max_x, sp.min_y, sp.max_z], [1.0, 0., 0.], [0., 1.0]),
-        // Left
-        ([sp.min_x, sp.min_y, sp.max_z], [-1.0, 0., 0.], [1.0, 0.]),
-        ([sp.min_x, sp.max_y, sp.max_z], [-1.0, 0., 0.], [0., 0.]),
-        ([sp.min_x, sp.max_y, sp.min_z], [-1.0, 0., 0.], [0., 1.0]),
-        ([sp.min_x, sp.min_y, sp.min_z], [-1.0, 0., 0.], [1.0, 1.0]),
-        // Top
-        ([sp.max_x, sp.max_y, sp.min_z], [0., 1.0, 0.], [1.0, 0.]),
-        ([sp.min_x, sp.max_y, sp.min_z], [0., 1.0, 0.], [0., 0.]),
-        ([sp.min_x, sp.max_y, sp.max_z], [0., 1.0, 0.], [0., 1.0]),
-        ([sp.max_x, sp.max_y, sp.max_z], [0., 1.0, 0.], [1.0, 1.0]),
-        // Bottom
-        ([sp.max_x, sp.min_y, sp.max_z], [0., -1.0, 0.], [0., 0.]),
-        ([sp.min_x, sp.min_y, sp.max_z], [0., -1.0, 0.], [1.0, 0.]),
-        ([sp.min_x, sp.min_y, sp.min_z], [0., -1.0, 0.], [1.0, 1.0]),
-        ([sp.max_x, sp.min_y, sp.min_z], [0., -1.0, 0.], [0., 1.0]),
-    ];
-
-    let positions: Vec<_> = vertices.iter().map(|(p, _, _)| *p).collect();
-    let normals: Vec<_> = vertices.iter().map(|(_, n, _)| *n).collect();
-    let uvs: Vec<_> = vertices
-        .iter()
-        .map(|(_, _, uv)| [uv[0] / 16., uv[1] / 16.])
-        .collect();
-
-    let indices = Indices::U32(vec![
-        0, 1, 2, 2, 3, 0, // front
-        4, 5, 6, 6, 7, 4, // back
-        8, 9, 10, 10, 11, 8, // right
-        12, 13, 14, 14, 15, 12, // left
-        16, 17, 18, 18, 19, 16, // top
-        20, 21, 22, 22, 23, 20, // bottom
-    ]);
-
-    let mesh = Mesh::new(PrimitiveTopology::TriangleList)
-        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, positions)
-        .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
-        .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uvs)
-        .with_indices(Some(indices));
-    handles.cube = meshes.add(mesh);
 
     // voxel world
     *load_voxel_world = LoadVoxelWorld::File("assets/monu9.vox".to_string());
 
-    // camera
+    // voxel camera
     commands.spawn((
         VoxelCameraBundle {
             transform: Transform::from_xyz(5.0, 5.0, -5.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -319,217 +160,9 @@ fn setup(
             }),
             ..default()
         },
-        // supports bloom and fxaa
         Fxaa::default(),
         FlyCam,
     ));
-}
-
-#[derive(Resource, Debug, Clone, PartialEq, Eq)]
-enum RenderMethod {
-    Naive,
-    Instanced,
-    ChunkedBlockMesh { greedy: bool },
-}
-impl Default for RenderMethod {
-    fn default() -> Self {
-        Self::ChunkedBlockMesh { greedy: true }
-    }
-}
-impl RenderMethod {
-    fn opts() -> impl IntoIterator<Item = Self> {
-        [
-            Self::Naive,
-            Self::Instanced,
-            Self::ChunkedBlockMesh { greedy: false },
-            Self::ChunkedBlockMesh { greedy: true },
-        ]
-        .into_iter()
-    }
-}
-
-fn start_benchmark(mut next_state: ResMut<NextState<FlowState>>) {
-    next_state.set(FlowState::Benchmark);
-}
-
-#[derive(Component)]
-struct BenchedMesh;
-
-fn setup_bench(
-    mut commands: Commands,
-    handles: Res<Handles>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    shape: Res<VoxelShape>,
-    method: Res<RenderMethod>,
-    mut toggle_wireframe: EventWriter<ToggleWireframeEvent>,
-    settings: Res<Settings>,
-) {
-    match *method {
-        RenderMethod::Naive => naive(&mut commands, handles, shape),
-        RenderMethod::Instanced => instanced(&mut commands, handles, shape),
-        RenderMethod::ChunkedBlockMesh { greedy } => {
-            chunked_block_mesh(&mut commands, handles, &mut meshes, shape, greedy)
-        }
-    }
-    toggle_wireframe.send(ToggleWireframeEvent {
-        active: settings.wireframe,
-    });
-}
-
-fn naive(commands: &mut Commands, handles: Res<Handles>, shape: Res<VoxelShape>) {
-    for pos in shape.iter() {
-        commands.spawn((
-            PbrBundle {
-                mesh: handles.cube.clone(),
-                material: handles.material.clone(),
-                transform: Transform::from_translation(pos.as_vec3()),
-                ..default()
-            },
-            BenchedMesh,
-        ));
-    }
-}
-
-fn instanced(commands: &mut Commands, handles: Res<Handles>, shape: Res<VoxelShape>) {
-    let vec: Vec<InstanceData> = shape
-        .iter()
-        .map(|pos| InstanceData {
-            position: pos.as_vec3() * Vec3::new(1., 1., 1.),
-            scale: 1.0,
-            color: Color::hsla(1.0, 0.0, 0.0, 1.0).as_rgba_f32(),
-        })
-        .collect();
-    let instances = InstanceMaterialData(vec);
-    commands.spawn((
-        handles.cube.clone(),
-        SpatialBundle::INHERITED_IDENTITY,
-        instances,
-        // If the camera doesn't see (0, 0, 0) all instances would be called.
-        bevy::render::view::NoFrustumCulling,
-        BenchedMesh,
-    ));
-}
-
-fn chunked_block_mesh(
-    commands: &mut Commands,
-    handles: Res<Handles>,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    shape: Res<VoxelShape>,
-    greedy: bool,
-) {
-    let grid = Grid::from_vec(shape.iter().collect());
-
-    type SampleShape = ConstShape3u32<CHUNK_SIDE_PADDED, CHUNK_SIDE_PADDED, CHUNK_SIDE_PADDED>;
-    for (pos, chunk) in grid.chunks.iter() {
-        let mut voxels = [EMPTY; SampleShape::SIZE as usize];
-        for (j, voxel) in chunk.0.iter().enumerate() {
-            let mut j = j as u32;
-            let z = j / CHUNK_AREA;
-            j -= z * CHUNK_AREA;
-            let y = j / CHUNK_SIDE;
-            let x = j % CHUNK_SIDE;
-            let voxel_pos = UVec3::new(x, y, z) + UVec3::splat(1);
-            let i = SampleShape::linearize(voxel_pos.to_array());
-            voxels[i as usize] = voxel.clone();
-        }
-
-        let faces = block_mesh::RIGHT_HANDED_Y_UP_CONFIG.faces;
-
-        let render_mesh = if greedy {
-            let mut buffer = block_mesh::GreedyQuadsBuffer::new(voxels.len());
-            greedy_quads(
-                &voxels,
-                &SampleShape {},
-                [0; 3],
-                [CHUNK_SIDE_PADDED - 1; 3],
-                &faces,
-                &mut buffer,
-            );
-            let num_indices = buffer.quads.num_quads() * 6;
-            let num_vertices = buffer.quads.num_quads() * 4;
-            let mut indices = Vec::with_capacity(num_indices);
-            let mut positions = Vec::with_capacity(num_vertices);
-            let mut normals = Vec::with_capacity(num_vertices);
-            for (group, face) in buffer.quads.groups.into_iter().zip(faces.into_iter()) {
-                for quad in group.into_iter() {
-                    indices.extend_from_slice(&face.quad_mesh_indices(positions.len() as u32));
-                    positions.extend_from_slice(&face.quad_mesh_positions(&quad.into(), 1.0));
-                    normals.extend_from_slice(&face.quad_mesh_normals());
-                }
-            }
-            let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList);
-            render_mesh.insert_attribute(
-                Mesh::ATTRIBUTE_POSITION,
-                VertexAttributeValues::Float32x3(positions),
-            );
-            render_mesh.insert_attribute(
-                Mesh::ATTRIBUTE_NORMAL,
-                VertexAttributeValues::Float32x3(normals),
-            );
-            render_mesh.insert_attribute(
-                Mesh::ATTRIBUTE_UV_0,
-                VertexAttributeValues::Float32x2(vec![[0.0; 2]; num_vertices]),
-            );
-            render_mesh.set_indices(Some(Indices::U32(indices.clone())));
-            render_mesh
-        } else {
-            let mut buffer = block_mesh::UnitQuadBuffer::new();
-            visible_block_faces(
-                &voxels,
-                &SampleShape {},
-                [0; 3],
-                [CHUNK_SIDE_PADDED - 1; 3],
-                &faces,
-                &mut buffer,
-            );
-            let num_indices = buffer.num_quads() * 6;
-            let num_vertices = buffer.num_quads() * 4;
-            let mut indices = Vec::with_capacity(num_indices);
-            let mut positions = Vec::with_capacity(num_vertices);
-            let mut normals = Vec::with_capacity(num_vertices);
-            for (group, face) in buffer.groups.into_iter().zip(faces.into_iter()) {
-                for quad in group.into_iter() {
-                    indices.extend_from_slice(&face.quad_mesh_indices(positions.len() as u32));
-                    positions.extend_from_slice(&face.quad_mesh_positions(&quad.into(), 1.0));
-                    normals.extend_from_slice(&face.quad_mesh_normals());
-                }
-            }
-            let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList);
-            render_mesh.insert_attribute(
-                Mesh::ATTRIBUTE_POSITION,
-                VertexAttributeValues::Float32x3(positions),
-            );
-            render_mesh.insert_attribute(
-                Mesh::ATTRIBUTE_NORMAL,
-                VertexAttributeValues::Float32x3(normals),
-            );
-            render_mesh.insert_attribute(
-                Mesh::ATTRIBUTE_UV_0,
-                VertexAttributeValues::Float32x2(vec![[0.0, 1.0]; num_vertices]),
-            );
-            render_mesh.set_indices(Some(Indices::U32(indices.clone())));
-            render_mesh
-        };
-
-        // i do not understand why Vec3::splat(1.5) is needed
-        let world_pos = pos.as_vec3() * Vec3::splat(CHUNK_SIDE as f32) - Vec3::splat(1.5);
-        let mesh_handle = meshes.add(render_mesh);
-        commands.spawn((
-            PbrBundle {
-                mesh: mesh_handle,
-                material: handles.material.clone(),
-                transform: Transform::from_translation(world_pos),
-                ..default()
-            },
-            BenchedMesh,
-        ));
-    }
-}
-
-fn teardown_bench(mut commands: Commands, query: Query<(Entity, &BenchedMesh)>) {
-    for (ent, _) in query.iter() {
-        commands.entity(ent).despawn_recursive()
-    }
 }
 
 // System for printing the number of meshes on every tick of the timer
