7e775de3771020143acf4a6ca4316e42d9fdc43e
Simone Shawn Cazzaniga <simone.shawn.cazzaniga@gmail.com>
2023-12-23 14:45:50 +0100

    I am here exclusively for satisfying my OCD

--- crates/voxel_storage/src/lib.rs
+++ crates/voxel_storage/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod chunk_map;
 pub mod grid;
 
-pub const CHUNK_SIDE: usize = 32;
-pub const CHUNK_AREA: usize = CHUNK_SIDE * CHUNK_SIDE;
-pub const CHUNK_VOLUME: usize = CHUNK_SIDE * CHUNK_SIDE * CHUNK_SIDE;
+pub const CHUNK_SIDE:   usize = 32;
+pub const CHUNK_AREA:   usize = CHUNK_SIDE * CHUNK_SIDE;
+pub const CHUNK_VOLUME: usize = CHUNK_AREA * CHUNK_SIDE;
