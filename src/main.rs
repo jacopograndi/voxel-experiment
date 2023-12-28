a4b159b83c55862bf7e94e83893e5ce17e450ac7
jacopograndi <jacopo.grandi@outlook.it>
2023-12-28 22:55:40 +0100

    boom gen

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 use bevy::{
     prelude::*,
-    utils::HashSet,
     window::{PresentMode, WindowPlugin},
 };
-
 use bevy_renet::{
     client_connected,
     transport::{NetcodeClientPlugin, NetcodeServerPlugin},
     RenetClientPlugin, RenetServerPlugin,
 };
-use mcrs_automata::lighting::recalc_lights;
 use mcrs_physics::plugin::VoxelPhysicsPlugin;
 use mcrs_render::{
     boxes_world::{VoxTextureIndex, VoxTextureLoadQueue},
-    voxel_world::VIEW_DISTANCE,
     VoxelRenderPlugin,
 };
-use mcrs_storage::{chunk::Chunk, universe::Universe, VoxelStoragePlugin, CHUNK_SIDE};
-use renet::{transport::NetcodeClientTransport, ClientId, RenetServer};
+use mcrs_storage::VoxelStoragePlugin;
+use renet::RenetServer;
 
 mod camera;
 mod diagnostics;
 mod input;
 mod net;
 mod terrain_editing;
+mod terrain_generation;
 
 use camera::*;
 use diagnostics::*;
@@ -37,6 +34,7 @@ use net::{
     *,
 };
 use terrain_editing::*;
+use terrain_generation::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -171,75 +169,4 @@ fn setup(mut commands: Commands, mut queue: ResMut<VoxTextureLoadQueue>) {
                 ..default()
             });
         });
-}
-
-fn gen_chunk(pos: IVec3) -> Chunk {
-    if pos.y < 0 {
-        Chunk::filled()
-    } else {
-        Chunk::empty()
-    }
-}
-
-fn get_chunks_in_sphere(pos: Vec3) -> HashSet<IVec3> {
-    let load_view_distance: u32 = VIEW_DISTANCE;
-
-    let camera_chunk_pos = (pos / CHUNK_SIDE as f32).as_ivec3() * CHUNK_SIDE as i32;
-    let load_view_distance_chunk = load_view_distance as i32 / CHUNK_SIDE as i32;
-    let lvdc = load_view_distance_chunk;
-
-    // sphere centered on pos
-    let mut chunks = HashSet::<IVec3>::new();
-    for x in -lvdc..=lvdc {
-        for y in -lvdc..=lvdc {
-            for z in -lvdc..=lvdc {
-                let rel = IVec3::new(x, y, z) * CHUNK_SIDE as i32;
-                if rel.as_vec3().length_squared() < load_view_distance.pow(2) as f32 {
-                    let pos = camera_chunk_pos + rel;
-                    chunks.insert(pos);
-                }
-            }
-        }
-    }
-    chunks
-}
-
-fn load_and_gen_chunks(
-    mut universe: ResMut<Universe>,
-    player_query: Query<(&NetPlayer, &Transform)>,
-    network_mode: Res<NetworkMode>,
-    transport: Option<Res<NetcodeClientTransport>>,
-) {
-    let client_id = if let Some(transport) = transport {
-        Some(ClientId::from_raw(transport.client_id()))
-    } else {
-        None
-    };
-
-    let players_pos = match *network_mode {
-        NetworkMode::Client => player_query
-            .iter()
-            .find(|(player, _)| client_id.map_or(false, |id| id == player.id))
-            .map_or(vec![], |(_, tr)| vec![tr.translation]),
-        _ => player_query
-            .iter()
-            .map(|(_, tr)| tr.translation)
-            .collect::<Vec<Vec3>>(),
-    };
-
-    let mut added = HashSet::<IVec3>::new();
-    for player_pos in players_pos.iter() {
-        let chunks = get_chunks_in_sphere(*player_pos);
-        for chunk_pos in chunks.iter() {
-            if let None = universe.chunks.get(chunk_pos) {
-                let chunk = gen_chunk(*chunk_pos);
-                universe.chunks.insert(*chunk_pos, chunk);
-                added.insert(*chunk_pos);
-            }
-        }
-    }
-
-    if !added.is_empty() {
-        recalc_lights(&mut universe, added.into_iter().collect());
-    }
 }
---
+++ src/terrain_generation.rs
@@ -0,0 +1,78 @@
+use bevy::{prelude::*, utils::HashSet};
+use mcrs_automata::lighting::recalc_lights;
+use mcrs_render::voxel_world::VIEW_DISTANCE;
+use mcrs_storage::{chunk::Chunk, universe::Universe, CHUNK_SIDE};
+use renet::{transport::NetcodeClientTransport, ClientId};
+
+use crate::net::{NetPlayer, NetworkMode};
+
+fn gen_chunk(pos: IVec3) -> Chunk {
+    if pos.y < 0 {
+        Chunk::filled()
+    } else {
+        Chunk::empty()
+    }
+}
+
+fn get_chunks_in_sphere(pos: Vec3) -> HashSet<IVec3> {
+    let load_view_distance: u32 = VIEW_DISTANCE;
+
+    let camera_chunk_pos = (pos / CHUNK_SIDE as f32).as_ivec3() * CHUNK_SIDE as i32;
+    let load_view_distance_chunk = load_view_distance as i32 / CHUNK_SIDE as i32;
+    let lvdc = load_view_distance_chunk;
+
+    // sphere centered on pos
+    let mut chunks = HashSet::<IVec3>::new();
+    for x in -lvdc..=lvdc {
+        for y in -lvdc..=lvdc {
+            for z in -lvdc..=lvdc {
+                let rel = IVec3::new(x, y, z) * CHUNK_SIDE as i32;
+                if rel.as_vec3().length_squared() < load_view_distance.pow(2) as f32 {
+                    let pos = camera_chunk_pos + rel;
+                    chunks.insert(pos);
+                }
+            }
+        }
+    }
+    chunks
+}
+
+pub fn load_and_gen_chunks(
+    mut universe: ResMut<Universe>,
+    player_query: Query<(&NetPlayer, &Transform)>,
+    network_mode: Res<NetworkMode>,
+    transport: Option<Res<NetcodeClientTransport>>,
+) {
+    let client_id = if let Some(transport) = transport {
+        Some(ClientId::from_raw(transport.client_id()))
+    } else {
+        None
+    };
+
+    let players_pos = match *network_mode {
+        NetworkMode::Client => player_query
+            .iter()
+            .find(|(player, _)| client_id.map_or(false, |id| id == player.id))
+            .map_or(vec![], |(_, tr)| vec![tr.translation]),
+        _ => player_query
+            .iter()
+            .map(|(_, tr)| tr.translation)
+            .collect::<Vec<Vec3>>(),
+    };
+
+    let mut added = HashSet::<IVec3>::new();
+    for player_pos in players_pos.iter() {
+        let chunks = get_chunks_in_sphere(*player_pos);
+        for chunk_pos in chunks.iter() {
+            if let None = universe.chunks.get(chunk_pos) {
+                let chunk = gen_chunk(*chunk_pos);
+                universe.chunks.insert(*chunk_pos, chunk);
+                added.insert(*chunk_pos);
+            }
+        }
+    }
+
+    if !added.is_empty() {
+        recalc_lights(&mut universe, added.into_iter().collect());
+    }
+}
